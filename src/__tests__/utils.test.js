67c94ccccc9247cf3ce9dbe08c8e104adec23003
victor <victor.mutuma@student.moringaschool.com>
2025-04-22 15:49:40 +0300

    save completed changes

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,16 @@
-import { currentAgeForBirthYear } from "../utils";
+import { currentAgeForBirthYear } from '../utils'; 
 
-describe("currentAgeForBirthYear", () => {
-  it("returns the age of a person based on the year of birth", () => {
-    const birthYear = 1984;
-    const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+describe('currentAgeForBirthYear', () => {
+  it('returns the age of a person based on the year of birth', () => {
+    const ageOfPerson = currentAgeForBirthYear(1986);
+    const currentYear = new Date().getFullYear();
+    const expectedAge = currentYear - 1986;
+    expect(ageOfPerson).toBe(expectedAge);
+  });
+
+  it('returns the current year for a person born in year 0', () => {
+    const ageOfPerson = currentAgeForBirthYear(0);
+    const currentYear = new Date().getFullYear();
+    expect(ageOfPerson).toBe(currentYear);
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,5 @@
-// Your code here
+export function currentAgeForBirthYear(birthYear) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - birthYear;
+  }
+  
